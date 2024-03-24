6095d13418f024a0e23f24399f3eb4821fc53c16
Raxmatilla Fayziyev <56291918+Raxmatilla97@users.noreply.github.com>
2024-03-25 02:04:39 +0500

    Update settings.py

--- translate/settings.py
+++ translate/settings.py
@@ -43,8 +43,9 @@ INSTALLED_APPS = [
     'trans'
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE = [  
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
